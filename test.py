28b4eae2c06d34d2cba1c88da49e0337f70af99b
Maciej Kluczyński <25069742+mklucz@users.noreply.github.com>
2017-12-29 23:30:26 +0100

    added class-maker helper code

--- test.py
+++ test.py
@@ -13,6 +13,8 @@ import yaml
 
 
 class RestClient:
+    
+
     entity_types = ['food', 'material', 'conflict', 'weapon', 'video_release',
     'performer', 'book', 'comics', 'occupation', 'episode', 'organization', 'magazine',
     'astronomical_object', 'platform', 'element', 'trading_card', 'trading_card_deck', 'company',
@@ -26,5 +28,46 @@ class RestClient:
     def __init__(self, url, apiKey):
         self.url = url
         self.apiKey = apiKey
-    def getAstronomicalObject(self):
-        return GetAstronomicalObject(self, url, apiKey)
+    # def getAstronomicalObject(self):
+    #     return GetAstronomicalObject(self, url, apiKey)
+
+
+
+entity_types = ['food', 'material', 'conflict', 'weapon', 'video_release',
+'performer', 'book', 'comics', 'occupation', 'episode', 'organization', 'magazine',
+'astronomical_object', 'platform', 'element', 'trading_card', 'trading_card_deck', 'company',
+'soundtrack', 'animal', 'trading_card_set', 'comic_series', 'spacecraft_type', 'genre',
+'medical_condition', 'video_game', 'technology', 'reference', 'spacecraft_class',
+'magazine_series', 'season', 'movie', 'spacecraft', 'book_collection', 'comic_strip',
+'staff', 'series', 'comic_collection', 'content_rating', 'title', 'content_language',
+'common', 'species', 'location', 'country', 'book_series', 'character', 'literature']
+
+klasy = []
+for entity in entity_types:
+    klasy.append(entity[:1].upper() + entity[1:])
+# print(klasy)
+lepsze_klasy = []
+for klasa in klasy:
+    for i in range(len(klasa)-2):
+        if klasa[i] == "_":
+            # print(klasa)
+            klasa = klasa[:i] + klasa[i+1].upper() + klasa[i+2:]
+            # print(klasa)
+            # print("\n")
+    lepsze_klasy.append(klasa)
+    # print(klasa)
+lepsze_klasy.sort()
+
+for klasa in lepsze_klasy:
+    print("class " + klasa + ":")
+    print("""
+    def __init__(self, url, apiKey):
+        self.url = url
+        self.apiKey = apiKey""")
+    print("""
+    def get(uid):
+        pass
+
+    def search(searchCriteria):
+        pass""")
+    print("\n")
