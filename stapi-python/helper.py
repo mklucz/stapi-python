df67df713d946e8f42c16f763ca60b77bcef61b2
Maciej Kluczyński <25069742+mklucz@users.noreply.github.com>
2018-01-04 23:10:45 +0100

    added init to helper_klasy_base

--- stapi-python/helper.py
+++ stapi-python/helper.py
@@ -1,8 +1,8 @@
 import urllib.request
 import json
 import os
-# import yaml
-
+import yaml
+import pprint
 # class AstronomicalObject:
 #     def __init__(self, uid):
 #         self.fetchedPage = urllib.request.urlopen("http://stapi.co/api/v1/rest/astronomicalObject?uid=" + str(uid))
@@ -24,27 +24,75 @@ entity_types = ['food', 'material', 'conflict', 'weapon', 'video_release',
 'magazine_series', 'season', 'movie', 'spacecraft', 'book_collection', 'comic_strip',
 'staff', 'series', 'comic_collection', 'content_rating', 'title', 'content_language',
 'common', 'species', 'location', 'country', 'book_series', 'character', 'literature']
+entity_types.sort()
 
-klasy = []
-for entity in entity_types:
-    klasy.append(entity[:1].upper() + entity[1:])
-# print(klasy)
-lepsze_klasy = []
-for klasa in klasy:
-    for i in range(len(klasa)-2):
-        if klasa[i] == "_":
-            # print(klasa)
-            klasa = klasa[:i] + klasa[i+1].upper() + klasa[i+2:]
-            # print(klasa)
-            # print("\n")
-    lepsze_klasy.append(klasa)
-    # print(klasa)
-lepsze_klasy.sort()
+lepsze_klasy = ['Animal', 'AstronomicalObject', 'Book', 'BookCollection', 'BookSeries', 'Character', 'ComicCollection',
+ 'ComicSeries', 'ComicStrip', 'Comics', 'Common', 'Company', 'Conflict', 'ContentLanguage', 'ContentRating', 'Country',
+  'Element', 'Episode', 'Food', 'Genre', 'Literature', 'Location', 'Magazine', 'MagazineSeries', 'Material',
+   'MedicalCondition', 'Movie', 'Occupation', 'Organization', 'Performer', 'Platform', 'Reference', 'Season', 'Series',
+    'Soundtrack', 'Spacecraft', 'SpacecraftClass', 'SpacecraftType', 'Species', 'Staff', 'Technology', 'Title',
+     'TradingCard', 'TradingCardDeck', 'TradingCardSet', 'VideoGame', 'VideoRelease', 'Weapon']
+
+snake_case = ['animal', 'astronomical_object', 'book', 'book_collection', 'book_series', 'character', 'comic_collection', 'comic_series', 'comic_strip', 'comics', 'common', 'company', 'conflict', 'content_language', 'content_rating', 'country', 'element', 'episode', 'food', 'genre', 'literature', 'location', 'magazine', 'magazine_series', 'material', 'medical_condition', 'movie', 'occupation', 'organization', 'performer', 'platform', 'reference', 'season', 'series', 'soundtrack', 'spacecraft', 'spacecraft_class', 'spacecraft_type', 'species', 'staff', 'technology', 'title', 'trading_card', 'trading_card_deck', 'trading_card_set', 'video_game', 'video_release', 'weapon']
+PascalCase = lepsze_klasy
+camelCase = [e[0].lower() + e[1:] for e in PascalCase]
+
+# klasy = []
+# for entity in entity_types:
+#     klasy.append(entity[:1].upper() + entity[1:])
+# # print(klasy)
+# lepsze_klasy = []
+# for klasa in klasy:
+#     for i in range(len(klasa)-2):
+#         if klasa[i] == "_":
+#             # print(klasa)
+#             klasa = klasa[:i] + klasa[i+1].upper() + klasa[i+2:]
+#             # print(klasa)
+#             # print("\n")
+#     lepsze_klasy.append(klasa)
+#     # print(klasa)
+# lepsze_klasy.sort()
+# print(lepsze_klasy)
+
+class AnimalBase:
+    def __init__(self, uid, name, earthAnimal=None):
+        pass
 
 def lower_first_char(s):
     return s[0].lower() + s[1:]
 # for lasa in lepsze_klasy:
 #     print("self." + lasa[0].lower() + lasa[1:] + " = " + lasa + "(url, apiKey)")
+
+def helper_klasy_base():
+    # base_file = open("base.py", "w")
+    for i in range(len(snake_case)):
+        output_code = "class " + PascalCase[i] + ":" + "\n" + "    def __init__(self, "
+        output_docstring = ""
+        loaded_yaml = yaml.load(open("/home/maciek/Documents/newprog/stapi/yaml/" +
+                                snake_case[i] + "/entity/" + snake_case[i] + "Base.yaml"))
+        for top_key, top_value in loaded_yaml.items():
+            if top_key == "type":
+                continue
+            elif top_key == "description":
+                continue
+            elif top_key == "properties":
+                positional_arguments = ""
+                named_arguments = ""
+                for key, value in sorted(top_value.items()):
+                    if "required" not in value:
+                        named_arguments += key + "=None, "
+                        print(named_arguments)
+                    else:
+                        positional_arguments += key + ", "
+                output_code += positional_arguments
+                output_code += named_arguments
+                output_code = output_code[:-2] + ")"
+                print(output_code)
+
+
+        break
+
+helper_klasy_base()
 
 def helper_klasy_full_response():
     full_response_file = open("full_response.py", "w")
@@ -56,7 +104,7 @@ def helper_klasy_full_response():
             self.""" + lower_first_char(klasa) + " = " + klasa + "Full(uid)")
 
         full_response_file.write("\n\n")
-helper_klasy_full_response()
+# helper_klasy_full_response()
 
 def helper_klasy_main():
     main_file = open("main.py", "w")
